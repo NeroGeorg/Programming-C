2066b1b37febd2c18e80e21d2b8ddfac999b0452
NeroGeorg <vip.nero228@mail.ru>
2025-03-04 15:18:32 +0700

    Add sort.c

---
+++ semestr2/lab3/sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// void InsertSort(struct student *new_student){
+//     for(int i = 1; i < 100; i++){
+        
+//         // struct student key = new_student[i];
+//         int j = i - 1;
+
+//         while ( j >= 0 && new_student[j].total < key.total){
+//             // new_student[j+1] = new_student[j];
+//             j --;
+//         }
+
+//         new_student[j+1] = key;
+//     }
+//     printf("Время выполнения: %f секунд\n", time_spent);
+// }
+
+int main(int, char**){
+    printf("Hello, from sort!\n");
+}
