807b1bf7b3a252e7e1136b15392c878722a59acc
Jora Barjomi <59428780+JoraBarjomi@users.noreply.github.com>
2025-05-09 20:15:46 +0700

    Delete semestr2/lab8 directory

--- semestr2/lab8/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jorik/Документы/Programming-C/semestr2/lab8
-# Build directory: /home/jorik/Документы/Programming-C/semestr2/lab8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(SortingTests "/home/jorik/Документы/Programming-C/semestr2/lab8/test_cmocka")
-set_tests_properties(SortingTests PROPERTIES  _BACKTRACE_TRIPLES "/home/jorik/Документы/Programming-C/semestr2/lab8/CMakeLists.txt;12;add_test;/home/jorik/Документы/Programming-C/semestr2/lab8/CMakeLists.txt;0;")
